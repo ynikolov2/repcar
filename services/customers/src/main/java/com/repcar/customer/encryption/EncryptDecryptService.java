2f7699599432f867eabcb0a9f12fd7c26eb33554
Miroslav Manchev <Miroslav.Manchev@bulpros.com>
2017-05-02 11:20:42 +0300

    New services.

---
+++ services/customers/src/main/java/com/repcar/customer/encryption/EncryptDecryptService.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright RepCar AD 2017
+ */
+package com.repcar.customer.encryption;
+
+public interface EncryptDecryptService {
+
+    String encrypt(String password);
+}
---
+++ services/viechles/src/main/java/com/repcar/viechle/encryption/EncryptDecryptService.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright RepCar AD 2017
+ */
+package com.repcar.viechle.encryption;
+
+public interface EncryptDecryptService {
+
+    String encrypt(String password);
+}
